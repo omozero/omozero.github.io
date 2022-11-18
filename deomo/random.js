7e93ea9b67ba6090845c418b90c9f53ac63be884
MoHai812 <liyacs69@gmail.com>
2022-11-18 23:23:37 +0800

    Site updated: 2022-11-18 23:23:33

--- deomo/random.js
+++ deomo/random.js
@@ -1,1 +1,1 @@
-var posts=["posts/81b6/","posts/8o4e/","posts/349b/","posts/810e/","posts/fe0c/","posts/a326/"];function toRandomPost(){pjax.loadUrl("/"+posts[Math.floor(Math.random()*posts.length)])}
+var posts=["posts/81b6/","posts/8o4e/","posts/810e/","posts/349b/","posts/a326/","posts/fe0c/"];function toRandomPost(){pjax.loadUrl("/"+posts[Math.floor(Math.random()*posts.length)])}
